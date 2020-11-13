62411e2996eea30336009ea99a09a74f9f054cda
moahmed eldereny <mohamed.a.eldereny@gmail.com>
2020-11-13 21:46:28 +0200

    init commit

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,63 @@
+import 'package:ecommerce/app_properties.dart';
+import 'package:ecommerce/screens/auth/welcome_back_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  Animation<double> opacity;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 2500), vsync: this);
+    opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward().then((_) {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void navigationPage() {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeBackPage()));
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/background.jpg'), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(color: transparentYellow),
+        child: SafeArea(
+          child: new Scaffold(
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Opacity(
+                      opacity: opacity.value,
+                      child: new Image.asset('assets/logo.png')),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
